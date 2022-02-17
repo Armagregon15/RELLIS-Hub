dc00efa778ad3dfff90b8255d931fc9021acdf96
joewulf1 <87823227+joewulf1@users.noreply.github.com>
2022-02-17 13:29:18 -0600

    Changed button

--- flutter_application_1/lib/setUp.dart
+++ flutter_application_1/lib/setUp.dart
@@ -1,14 +1,15 @@
 // ignore: file_names
 // ignore_for_file: prefer_const_constructors, avoid_print, file_names
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/answer.dart';
+/*import 'package:flutter_application_1/answer.dart';
 import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+*/
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import './result.dart';
-import './quiz.dart';
-import './answer.dart';
 
 // ignore: use_key_in_widget_constructors
 class SetUp extends StatefulWidget {
@@ -24,7 +25,7 @@ class SetUpState extends State<SetUp> {
   List<dynamic> savedWordPairs = [];
   static List<int> answers = [];
   final _questions = [
-    // this is a map for the quuestion and the answers. Key value pairs
+    // this is a map for the question and the answers. Key value pairs
     {
       'questionText': 'Pick Your Univeristy',
       'answers': [
@@ -64,7 +65,14 @@ class SetUpState extends State<SetUp> {
     },
   ];
   var _questionIndex = 0;
+  int _selectedIndex = 0;
   //var saveAnswers = [];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   static void saveAnswer(int value) {
     answers.add(value);
@@ -89,27 +97,139 @@ class SetUpState extends State<SetUp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color(0xFF500000),
-          title: const Text('The Hub @ Rellis'),
+        home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xFF500000),
+        title: const Text('The Hub @ Rellis'),
+      ),
+      body: _questionIndex <
+              _questions.length //checks to see if all questions are answered
+          ? Quiz(
+              answerQuestion: _answerQuestion,
+              questionIndex: _questionIndex,
+              questions: _questions,
+            )
+          : MainPage(answers),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 15,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        selectedIconTheme: IconThemeData(
+          color: Colors.white,
+          size: 35,
         ),
-        body: _questionIndex <
-                _questions.length //checks to see if all questions are answered
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-                questions: _questions,
-              )
-            : MainPage(answers),
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: const Color(0xFF500000),
+        elevation: 90,
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            label: 'Return',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.arrow_forward,
+              color: Colors.white,
+            ),
+            label: 'Next',
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  // ignore: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        // ignore: prefer_const_constructors
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
       ),
     );
   }
 }
 
-@override
-Widget build(BuildContext context) {
-  // TODO: implement build
-  throw UnimplementedError();
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final VoidCallback answerQuestion;
+
+  Quiz({
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        Question(
+          questions[questionIndex]['questionText']?.toString() ?? '',
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(answerQuestion, answer['text'].toString(),
+              answer['value'].toString());
+        }).toList()
+      ],
+    );
+  }
+}
+
+class Answer extends StatefulWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+  final String value;
+  // ignore: prefer_const_constructors_in_immutables, use_key_in_widget_constructors
+  Answer(this.selectHandler, this.answerText, this.value);
+
+  @override
+  State<Answer> createState() => _AnswerState();
+}
+
+class _AnswerState extends State<Answer> {
+  bool click = true;
+
+  get() => widget.value;
+
+  bool boolCheck = false;
+  bool newValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: CheckboxListTile(
+        title: Text(widget.answerText, style: TextStyle(fontSize: 18.0)),
+        value: boolCheck,
+        onChanged: (bool? newValue) {
+          setState(() {
+            boolCheck = !boolCheck;
+          });
+          SetUpState.saveAnswer(int.parse(get()));
+          print(int.parse(get()));
+        },
+        secondary: const Icon(Icons.dangerous),
+      ),
+    );
+  }
 }
