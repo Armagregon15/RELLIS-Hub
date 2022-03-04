66ebd8edabecab7bc4faf818d82ccdd6002ca8e3
joewulf1 <joe.hiking@gmail.com>
2022-03-03 20:36:31 -0600

    Cleanup

--- flutter_application_1/lib/setUp.dart
+++ flutter_application_1/lib/setUp.dart
@@ -7,7 +7,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import './result.dart';
 import 'firebase_options.dart';
-import './HomePage.dart';
 
 // ignore: use_key_in_widget_constructors
 class SetUp extends StatefulWidget {
