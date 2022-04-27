0fd10fbac75e5f023d1d2db618ed1601de0b74ea
Nicholas Garcia <nrgnrg12@ymail.com>
2022-04-27 14:49:17 -0500

    update
    update

--- flutter_application_1/lib/authmain.dart
+++ flutter_application_1/lib/authmain.dart
@@ -63,6 +63,7 @@ class AuthService {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User user = result.user!;
+      indexdb = [18];
       await DatabaseService(uid: user.uid).updateUserData(indexdb);
       return _userFromFirebaseUser(user);
     } catch (error) {
